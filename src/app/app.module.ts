1518f55a55660db8639194fecf806f9a0c4d3afc
sunilmondal4you <sunilmondal4you@gmail.com>
2023-06-24 16:19:58 +0530

    pages update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UserComponent } from './user/user.component';
-import { PostComponent } from './post/post.component';
+import { HomeComponent } from './components/home/home.component';
 import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    UserComponent,
-    PostComponent,
     UsersComponent
   ],
   imports: [
