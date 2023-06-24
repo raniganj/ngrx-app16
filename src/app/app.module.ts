f82b2f031ccf9691a1612918772791cba06ec61d
sunilmondal4you <sunilmondal4you@gmail.com>
2023-06-24 16:03:58 +0530

    component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
