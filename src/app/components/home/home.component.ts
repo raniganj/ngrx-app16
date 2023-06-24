91e3b2e20985cfbe364f167d68982d2019f93ce0
sunilmondal4you <sunilmondal4you@gmail.com>
2023-06-24 18:43:21 +0530

    home, user added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { incrementCounter, incrementCounterByParam } from 'src/app/store/counter.reducer';
+import { MyAppState } from 'src/app/store/root.reducer';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  title = 'ngrx-app';
+  counter:number|undefined;
+  inputValue:number = 0;
 
+  constructor(private store:Store<MyAppState>) { }
+
+  ngOnInit(): void {
+    this.store.subscribe((state)=>{
+      this.counter = state.counter;
+    })
+  }
+
+  incrementCounter(){
+    this.store.dispatch(incrementCounter())
+  }
+
+  incrementCounterByParam(){
+    this.store.dispatch(incrementCounterByParam({val:this.inputValue}))
+  }
 }
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { MyAppState } from 'src/app/store/root.reducer';
+import { UserData1, userData1Increament, userData1IncreamentByParams } from 'src/app/store/user.reducer';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
 
+  userData: UserData1 | undefined;
+  inputValue:number = 0;
+
+  constructor(private store:Store<MyAppState>) { }
+
+  ngOnInit(): void {
+    this.store.subscribe((state)=>{
+      this.userData = state.userData1;
+    })
+  }
+
+  userData1Increament(){
+    this.store.dispatch(userData1Increament());
+  }
+
+  userData1IncreamentByParams(){
+    this.store.dispatch(userData1IncreamentByParams({val : this.inputValue}))
+  }
 }
