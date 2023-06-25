f7722c72502f329fe10a0e60fb6e8523e7988f6b
sunilmondal4you <sunilmondal4you@gmail.com>
2023-06-25 15:31:35 +0530

    rename

--- src/app/store/score.reducer.ts
+++ src/app/store/score.reducer.ts
@@ -9,15 +9,19 @@ export interface ScoreStore {
   score?: ScoreModel;
 }
 
-export const team1ScoreIncreament = createAction('On Team1 Score Increament');
-export const mumScoreincWithPayload = createAction(
-  '[On Team1 Score Increament With paylaod]',
+export const team1ScoreIncreamentAction = createAction(
+  'On Team1 Score Increament Action'
+);
+export const mumScoreincWithPayloadAction = createAction(
+  '[On Team1 Score Increament With paylaod Action]',
   props<{ incrementBy: number }>()
 );
 
-export const team2ScoreIncreament = createAction('On Team2 Score Increament');
-export const team2ScoreIncWithPayload = createAction(
-  'On Team2 Score Increament With paylaod',
+export const team2ScoreIncreamentAction = createAction(
+  'On Team2 Score Increament Action'
+);
+export const team2ScoreIncWithPayloadAction = createAction(
+  'On Team2 Score Increament With paylaod Action',
   props<{ incrementBy: number }>()
 );
 
@@ -31,25 +35,25 @@ export const initialState: ScoreStore = {
 export const scoreReducer = createReducer(
   initialState,
 
-  on(team1ScoreIncreament, (state) => {
+  on(team1ScoreIncreamentAction, (state) => {
     let team1Score = state.score?.team1Score || 0;
     let newscore = { ...state.score, team1Score: team1Score + 1 };
     return { ...state, score: newscore };
   }),
 
-  on(mumScoreincWithPayload, (state, { incrementBy }) => {
+  on(mumScoreincWithPayloadAction, (state, { incrementBy }) => {
     let team1Score = state.score?.team1Score || 0;
     let newscore = { ...state.score, team1Score: team1Score + incrementBy };
     return { ...state, score: newscore };
   }),
 
-  on(team2ScoreIncreament, (state) => {
+  on(team2ScoreIncreamentAction, (state) => {
     let team2Score = state.score?.team2Score || 0;
     let newscore = { ...state.score, team2Score: team2Score + 1 };
     return { ...state, score: newscore };
   }),
 
-  on(team2ScoreIncWithPayload, (state, { incrementBy }) => {
+  on(team2ScoreIncWithPayloadAction, (state, { incrementBy }) => {
     let team2Score = state.score?.team2Score || 0;
     let newscore = { ...state.score, team2Score: team2Score + incrementBy };
     return { ...state, score: newscore };
